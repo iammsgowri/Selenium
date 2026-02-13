f8514f7cdea7914be5f5fd10aaf88ed4aacba22d
Gowri <Gowri@192.168.1.3>
2026-02-13 10:50:13 +0530

    script to launch and quit chrome browser

---
+++ Selenium/src/test/java/seleniumbrowserdrivers/ChromeBrowserDriver.java
@@ -0,0 +1,14 @@
+package seleniumbrowserdrivers;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserDriver {
+
+	public static void main(String[] args) {
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.quit();
+
+	}
+
+}
