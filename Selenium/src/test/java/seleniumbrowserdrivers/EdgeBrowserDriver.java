666bbdb19a9d8348347e54a11a00aa024a9a22ac
Gowri <Gowri@192.168.1.3>
2026-02-13 10:52:26 +0530

    script to launch and quit edge browser

---
+++ Selenium/src/test/java/seleniumbrowserdrivers/EdgeBrowserDriver.java
@@ -0,0 +1,14 @@
+package seleniumbrowserdrivers;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowserDriver {
+
+	public static void main(String[] args) {
+
+		EdgeDriver driver = new EdgeDriver();
+		driver.quit();
+
+	}
+
+}
