03d582bb2833ecdd53d2cd6afb3e04331bd703fb
Gowri <Gowri@192.168.1.3>
2026-02-13 10:54:26 +0530

    script to launch and quit firefox browser

---
+++ Selenium/src/test/java/seleniumbrowserdrivers/FirefoxBrowserDriver.java
@@ -0,0 +1,14 @@
+package seleniumbrowserdrivers;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxBrowserDriver {
+
+	public static void main(String[] args) {
+
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.quit();
+
+	}
+
+}
